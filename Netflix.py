240ff5ceb240bbed21a90bd0d9372ac8d547d543
Varun Parameswaran <vparam15@gmail.com>
2014-09-21 15:18:46 -0500

    Netflix.py updated with imported caches for average movie ratings and average user ratings

--- Netflix.py
+++ Netflix.py
@@ -1,6 +1,6 @@
-def main ():
+import pickle
 	# readin the probe.txt file to standard in
-	def netflix_read (r):
+def netflix_read (r):
 	index = 0
 	s = r.readlines()
 	numlines = len(s)
@@ -20,14 +20,30 @@ def main ():
 	return (netflix_dict)
 
 		
-	import pickle
-	def netflix_predict():
-	# read the key (movie title) from the dictionairy created
-	for movid in netflix_dict:
+	
+def netflix_predict():
 
-		with open('/u/prat0318/netflix-tests/savant-cacheMovies.txt','r') as f:
-		j = pickle.load(f) OR eval(f.readline())
-	print (j)
+	with open('/u/prat0318/netflix-tests/savant-cacheMovies.txt','r') as f: #This accesses the average movie rating file created by savant
+		j = f.readlines()
+	avgmovie_dict = {}
+	for i in j:
+		j1 = i.split()
+		avgmovie_dict[int(j1[0])] = float(j1[1]) #populate a dictionairy with movie id and average rating 		
+
+
+	with open('/u/prat0318/netflix-tests/savant-cacheUsers.txt','r') as f: # This accesses the average user rating file created by savant
+		k = f.readlines()
+
+	avguser_dict = {}
+	for m in k:
+		k1 = m.split()
+		k11 = k1[0].strip('\x00') #This was added to remove the trailing characters at the end of the text file
+		k12 = k1[1].strip('\x00')
+		avguser_dict[int(k11)] = float(k12) # Populated dictionairy with customer id and average customer rating
+
+
+
+	# read the key (movie title) from the dictionairy created
 	# go through savant-cachemovies.txt and find average movie rating 
 	# read  the associated values (customer ids) for the movie and find their average rating
 	# for each value, predict a weighted guess (using average customer rating and movie rating)
@@ -35,10 +51,10 @@ def main ():
 	#read movie title -> find the average rating for that movie through the cache
 	#read customer id and find corresponding avg customer id rating through cache
 	# predict a rating based on the above 2 averages. Try using different weights 
-	def netflix_calc():
+def netflix_calc():
 
 	#run the predictiion against the real value using RMSE.py
 
-	def netflix_write():
+def netflix_write():
 		#output movie id followed by predicted ratings instead of customer ids
 		#ouptut file to netflix.out
