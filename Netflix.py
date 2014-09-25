f053c65a431f62ef349154ea0ad62551124d5a0a
Varun Parameswaran <vparam15@gmail.com>
2014-09-24 19:49:19 -0500

    Transferred changes from draft (Readfile.py) and broke code up into functions inside Netflix.py

--- Netflix.py
+++ Netflix.py
@@ -1,10 +1,22 @@
-import pickle
-	# readin the probe.txt file to standard in
-def netflix_read (r):
+import math
+netflix_dict = {}
+avgmovie_dict = {}
+avguser_dict = {}
+predict_dict = {}
+def rmse(a,p):
+	assert (len(a) == len(p))
+	sum1 = 0
+	for i in range(len(a)):
+		sum1 += (a[i] - p[i])**2
+	mean = sum1/len(a)
+	r = math.sqrt(mean)
+	return (r)
+
+def netflix_read (r1):
+	r = open(r1,'r')	# readin the probe.txt file to standard in
 	index = 0
 	s = r.readlines()
 	numlines = len(s)
-	netflix_dict = {}
 	movid = 0
 
 	while index < numlines:	# read the movie id which has a ':'
@@ -19,42 +31,68 @@ def netflix_read (r):
 
 	return (netflix_dict)
 
-def rmse(a,p):
-	assert (len(a) == len(p))
-	sum1 = 0
-	for i range(len(a)):
-		sum1 += (a[i] - p[i])**2
-	mean = sum1/len(a)
-	r = math.sqrt(mean)
-	return r	
-	
-def netflix_predict():
 
-	with open('/u/prat0318/netflix-tests/savant-cacheMovies.txt','r') as f: #This accesses the average movie rating file created by savant
+
+def netflix_moviecache(r2):
+	with open(r2,'r') as f: #This accesses the average movie rating file created by savant
+		j = eval(f.read())
+	for i in j.keys():
+		avgmovie_dict[int(i)] = float(j[i]) #populate a dictionairy with movie id and average rating 		
+	return (avgmovie_dict)
+
+def netflix_usercache(r3):	
+	f = open(r3,'r')
+	r = eval(f.read())
+	for u in r.keys():
+		avguser_dict[int(u)] = float(r[u])
+	return (avguser_dict)
+
+def netflix_panswers(r4):
+	with open(r4,'r') as f: #This accesses the average movie rating file created by savant
 		j = f.readlines()
-	avgmovie_dict = {}
-	for i in j:
-		j1 = i.split()
-		avgmovie_dict[int(j1[0])] = float(j1[1]) #populate a dictionairy with movie id and average rating 		
+
+	for q in j:
+		q1 = q.split()
+		movID, cusID, rat = int(q1[0]),int(q1[1]),int(q1[2])
+		if (movID in predict_dict.keys()):
+			predict_dict[movID][cusID] = rat
+		else:
+			predict_dict[movID] = {cusID : rat}
+	return (predict_dict)
+
+def main():
+	netflix_read('/u/downing/cs/netflix/probe.txt')
+	netflix_moviecache('/u/prat0318/netflix-tests/savant-cacheMovies.txt')
+	netflix_usercache('/u/prat0318/netflix-tests/savant-cacheUsers.txt')
+	netflix_panswers('/u/prat0318/netflix-tests/cct667-ProbeCacheAnswers.txt')
+	
+	m = 0
+	c = 0
+	w1 = 0.5
+	a = []
+	p = []
 
 
+	for x in netflix_dict.keys():
+		try:
+			m = avgmovie_dict[int(x)]
+		except KeyError:
+			m = 3.7
+		for y in netflix_dict[x]:
+			try:
+				c = avguser_dict[int(y)]
+			except KeyError:
+				c = 3.7
+			prediction = 3.7 + w1*(m-3.7) + (1-w1)*(c-3.7)	
+			a.append(prediction) 
+		for x1 in (predict_dict[x].keys()):
+			p.append(predict_dict[x][x1])
+	print (rmse(a,p))
 
-	f = open('/u/prat0318/netflix-tests/ctd446-userAverageRating.txt')
-	r = f.read()
-	tmp = r.split(',')
-	tmp[0] = tmp[0].replace('{', '')
-	tmp[-1] = tmp[1].replace('}', '')
-	avguser_dict = {}
-	for i in tmp:
-		tmp2 = i.split(':')
-		tmp2[0] = tmp2[0].replace('"', '')
-		tmp2[1] = tmp2[1].replace('"', '')
-		custID = int(tmp2[0].replace(' ', ''))
-		rating = float(tmp2[1].replace(' ', ''))
-		avguser_dict[custID] = rating
+main()	
 
 	# with open('/u/prat0318/netflix-tests/savant-cacheUsers.txt','r') as f: # This accesses the average user rating file created by savant
-	# 	k = f.readlines()
+	# 	k = f.readlines()+
 
 	# avguser_dict = {}
 	# for m in k:
@@ -73,10 +111,10 @@ def netflix_predict():
 	#read movie title -> find the average rating for that movie through the cache
 	#read customer id and find corresponding avg customer id rating through cache
 	# predict a rating based on the above 2 averages. Try using different weights 
-def netflix_calc():
+# def netflix_calc():
 
-	#run the predictiion against the real value using RMSE.py
+# 	#run the predictiion against the real value using RMSE.py
 
-def netflix_write():
+# def netflix_write():
 		#output movie id followed by predicted ratings instead of customer ids
 		#ouptut file to netflix.out
